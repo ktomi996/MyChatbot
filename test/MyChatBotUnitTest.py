0ecaa35dbf38ad1cb82966d7ee32610e8f29da88
Koller Tamas <koller.tamas1996@gmail.com>
2023-08-29 16:46:06 +0200

    Create Unittests for MyChatbot class 	1. The OuterChatApiTestPostAndGetMethods checks only the methods which called without using http requests 	2. The OuterChatApiTestHTTPReqs checks the get and post http endpoints

---
+++ test/MyChatBotUnitTest.py
@@ -0,0 +1,61 @@
+import sys
+sys.path.append('../source')
+from unittest import TestCase
+from MyChatbot import MyChatbot
+from MyChatbot import GreetingModel
+from typing_extensions import Annotated
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
+from fastapi import Depends
+import requests
+from multiprocessing import Process
+import asynctest
+import asyncio
+import aiohttp
+import uvicorn
+import time
+from MockClasses import MockChat
+from TestFunctions import send_post_request, get_response
+
+
+class OuterChatApiTestPostAndGetMethods(TestCase):
+    def setUp(self):
+        self.my_outer_chat_ai = MockChat()
+        self.security = HTTPBasic()
+        self.app = MyChatbot(self.my_outer_chat_ai)
+    def test_get_and_post_method(self):
+        credentials = Annotated[HTTPBasicCredentials, Depends(self.security)]
+        credentials.username = "koller"
+        credentials.password = "tamas"
+        for i in range(0,10):
+            text = GreetingModel(texts = str(i))
+            self.app.post_method(text, credentials)
+        result = self.app.get_method()
+        assert result["message"] == ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'], "Bad result"
+
+class OuterChatApiTestHTTPReqs(asynctest.TestCase):
+        async def setUp(self):
+            self.my_outer_chat_ai = MockChatGPT()
+            self.security = HTTPBasic()
+            self.app = MyChatbot(self.my_outer_chat_ai)
+            self.proc = Process(target=uvicorn.run,
+                            args=(self.app.app,),
+                            kwargs={
+                                "host": "127.0.0.1",
+                                "port": 8000,
+                                "log_level": "info"},
+                            daemon=True)
+            self.proc.start()
+            await asyncio.sleep(0.1)  # time for the server to start
+        async def tearDown(self):
+            """ Shutdown the app. """
+            self.proc.terminate()
+
+
+
+        def test_get_and_post_method(self):
+            for i in range(0,10):
+                response = send_post_request(str(i))
+                assert response.status_code == 200, "Bad status code in post"
+            time.sleep(0.1)   
+            assert response.status_code == 200, "Bad status code in get"
+            assert response.content == b'{"message":["0","1","2","3","4","5","6","7","8","9"]}', "Bad response in get"
