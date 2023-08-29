3a6c1a4c663a966364ee70dedb9ae9343640c706
Koller Tamas <koller.tamas1996@gmail.com>
2023-08-29 16:50:17 +0200

    Integration test for MyChatBot and Chat classes

---
+++ test/IntegrationTest.py
@@ -0,0 +1,44 @@
+import sys
+sys.path.append('../source')
+from unittest import TestCase
+from MyChatbot import MyChatbot
+from MyChatbot import GreetingModel
+import requests
+from multiprocessing import Process
+import asynctest
+import asyncio
+import aiohttp
+import uvicorn
+import time
+from MockClasses import MockChatGPT
+from ChatGPTInterface import Chat
+from TestFunctions import send_post_request, get_response
+
+
+class IntegrationTest1(asynctest.TestCase):
+        async def setUp(self):
+            self.my_outer_chat_ai = Chat(MockChatGPT())
+            self.app = MyChatbot(self.my_outer_chat_ai)
+            self.proc = Process(target=uvicorn.run,
+                            args=(self.app.app,),
+                            kwargs={
+                                "host": "127.0.0.1",
+                                "port": 8000,
+                                "log_level": "info"},
+                            daemon=True)
+            self.proc.start()
+            await asyncio.sleep(0.1)  # time for the server to start
+        async def tearDown(self):
+            """ Shutdown the app. """
+            self.proc.terminate()
+
+
+
+        def test_get_and_post_method(self):
+            for i in range(0,10):
+                response = send_post_request(str(i))
+                assert response.status_code == 200, "Bad status code in post"
+            time.sleep(0.1)   
+            response = get_response()
+            assert response.status_code == 200, "Bad status code in get"
+            assert response.content == b'{"message":["0","1","2","3","4","5","6","7","8","9"]}', "Bad response in get"
